9067e48b7548beb2c7d2e8aab4f0b1cea9e4f44b
Nader <nadermhd@hotmail.com>
2021-08-13 14:32:57 +0200

    graph.py is updated and shortest_path function was added

--- graph.py
+++ graph.py
@@ -7,6 +7,7 @@ Weighted graph: where edges have certain weight value to indicate its importance
 
 '''
 
+
 class Graph():
     def __init__(self, edges):
         self.edges = edges
@@ -21,6 +22,47 @@ class Graph():
                 self.graph_dict[start] = [end]
         print('Graph dict:', self.graph_dict)
 
+    def get_paths(self, start, end, path=[]):
+        # one path at every run
+        path = path + [start]
+
+        if start == end:
+            return [path]
+        
+        if start not in self.graph_dict:
+            return []
+
+        paths = [] # all possible paths
+        for node in self.graph_dict[start]:
+            if node not in path:
+                new_paths = self.get_paths(node, end, path)
+                for p in new_paths:
+                    paths.append(p)
+        
+        return paths
+
+    def shortest_path(self, start, end):
+        # EITHER
+        paths = self.get_paths(start, end)
+        #shortest path
+        sp = 0
+        sort_p = []
+        import numpy as np
+        
+        for pp in paths:
+            
+            if len(pp) <= sp:
+                sp = len(pp) 
+                sort_p.append(pp)
+            else:
+                sort_p = pp
+        
+        # OR
+        min_ = np.argmin(paths)
+        print(paths[min_])
+
+        return sort_p
+
 if __name__ == '__main__':
     routes = [
                 ('Mumbai', 'Paris'),
@@ -31,3 +73,10 @@ if __name__ == '__main__':
                 ('Newyork', 'Tornoto') 
              ]
     route_graph = Graph(routes)
+    
+    start = 'Mumbai'
+    end = 'Newyork' 
+    print(f"\nPath between {start} and {end}", route_graph.get_paths(start, end))
+
+    print(f"\nSortest path between {start} and {end}", route_graph.shortest_path(start, end))
+    print('\nend')
